72e57d610dcf1e183c9d5b1600596eda96de7a7a
Kostas Anastasiou <anastasiou.ks@gmail.com>
2023-06-06 12:28:19 +0300

    Removed Bootstrap Lib and added custom CSS for SideMenu

--- src/component/Header/Header.component.tsx
+++ src/component/Header/Header.component.tsx
@@ -1,5 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import Link from 'Component/Link';
 import { ReactElement } from 'Type/Common.type';
 import { HeaderComponent as SourceHeaderComponent } from 'SourceComponent/Header/Header.component';
@@ -15,53 +13,24 @@ export class HeaderComponent extends SourceHeaderComponent {
             return null;
         }
 
-        if (isMobile) {
-            return (
-                <>
-                    <SideMenu />
-                    <Link
-                      to="/"
-                      aria-label="Go to homepage by clicking on ScandiPWA logo"
-                      aria-hidden={ !isVisible }
-                      tabIndex={ isVisible ? 0 : -1 }
-                      block="Header"
-                      elem="LogoWrapper"
-                      mods={ { isVisible } }
-                      key="logo"
-                    >
-                        { this.renderLogoImage() }
-                    </Link>
-                </>
-            );
-        }
-
         return (
-            <Link
-              to="/"
-              aria-label="Go to homepage by clicking on ScandiPWA logo"
-              aria-hidden={ !isVisible }
-              tabIndex={ isVisible ? 0 : -1 }
-              block="Header"
-              elem="LogoWrapper"
-              mods={ { isVisible } }
-              key="logo"
-            >
-            { this.renderLogoImage() }
-            </Link>
+            <>
+                { isMobile && <SideMenu /> }
+                <Link
+                  to="/"
+                  aria-label="Go to homepage by clicking on ScandiPWA logo"
+                  aria-hidden={ !isVisible }
+                  tabIndex={ isVisible ? 0 : -1 }
+                  block="Header"
+                  elem="LogoWrapper"
+                  mods={ { isVisible } }
+                  key="logo"
+                >
+                { this.renderLogoImage() }
+                </Link>
+            </>
         );
     }
-
-    // renderMenu(): ReactElement {
-    //     const { isCheckout, device: { isMobile } } = this.props;
-
-    //     if (isMobile || isCheckout) {
-    //         return (
-    //             <SidebarContent />
-    //         );
-    //     }
-
-    //     return <Menu />;
-    // }
 }
 
 export default HeaderComponent;
